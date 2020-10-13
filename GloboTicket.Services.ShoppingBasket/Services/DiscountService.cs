b4c27040d00a2144b5d2a3f3220445480c02f8c9
Dan Sabadis <dsabadis@sdl.com>
2020-10-13 11:32:06 +0300

    done

--- GloboTicket.Services.ShoppingBasket/Services/DiscountService.cs
+++ GloboTicket.Services.ShoppingBasket/Services/DiscountService.cs
@@ -9,63 +9,63 @@ namespace GloboTicket.Services.ShoppingBasket.Services
 {
     public class DiscountService: IDiscountService
     {
-        //private readonly HttpClient client;
+        private readonly HttpClient client;
 
-        //public DiscountService(HttpClient client)
-        //{
-        //    this.client = client;
-        //    //this.client.DefaultRequestHeaders.Add("Accept", "...");
-        //    //this.client.BaseAddress = new Uri("...");
-        //}
-
-        //public async Task<Coupon> GetCoupon(Guid couponId)
-        //{
-        //    var response = await client.GetAsync($"/api/discount/{couponId}");
-        //    return await response.ReadContentAs<Coupon>();
-        //}
-
-        //public async Task<Coupon> GetCouponWithError(Guid couponId)
-        //{
-        //    var response = await client.GetAsync($"/api/discount/error/{couponId}");
-        //    return await response.ReadContentAs<Coupon>();
-        //}
-        private readonly Discounts.DiscountsClient discountsService;
-
-        public DiscountService(Discounts.DiscountsClient discountsService)
+        public DiscountService(HttpClient client)
         {
-            this.discountsService = discountsService;
+            this.client = client;
+            //this.client.DefaultRequestHeaders.Add("Accept", "...");
+            //this.client.BaseAddress = new Uri("...");
         }
 
         public async Task<Coupon> GetCoupon(Guid couponId)
         {
-            try
-            {
-                GetCouponByIdRequest getCouponByIdRequest = new GetCouponByIdRequest { CouponId = couponId.ToString() };
-
-                GetCouponByIdResponse getCouponByIdResponse = await discountsService.GetCouponAsync(getCouponByIdRequest);
-
-                var coupon = new Coupon
-                {
-                    Code = getCouponByIdResponse.Coupon.Code,
-                    Amount = getCouponByIdResponse.Coupon.Amount,
-                    AlreadyUsed = getCouponByIdResponse.Coupon.AlreadyUsed,
-                    CouponId = Guid.Parse(getCouponByIdResponse.Coupon.CouponId)
-                };
-
-                return coupon;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-
+            var response = await client.GetAsync($"/api/discount/{couponId}");
+            return await response.ReadContentAs<Coupon>();
         }
 
-        public Task<Coupon> GetCouponWithError(Guid couponId)
+        public async Task<Coupon> GetCouponWithError(Guid couponId)
         {
-            //not needed for this demo here
-            throw new NotImplementedException();
+            var response = await client.GetAsync($"/api/discount/error/{couponId}");
+            return await response.ReadContentAs<Coupon>();
         }
+        ////private readonly Discounts.DiscountsClient discountsService;
+
+        //public DiscountService(Discounts.DiscountsClient discountsService)
+        //{
+        //    this.discountsService = discountsService;
+        //}
+
+        //public async Task<Coupon> GetCoupon(Guid couponId)
+        //{
+        //    try
+        //    {
+        //        GetCouponByIdRequest getCouponByIdRequest = new GetCouponByIdRequest { CouponId = couponId.ToString() };
+
+        //        GetCouponByIdResponse getCouponByIdResponse = await discountsService.GetCouponAsync(getCouponByIdRequest);
+
+        //        var coupon = new Coupon
+        //        {
+        //            Code = getCouponByIdResponse.Coupon.Code,
+        //            Amount = getCouponByIdResponse.Coupon.Amount,
+        //            AlreadyUsed = getCouponByIdResponse.Coupon.AlreadyUsed,
+        //            CouponId = Guid.Parse(getCouponByIdResponse.Coupon.CouponId)
+        //        };
+
+        //        return coupon;
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e);
+        //        throw;
+        //    }
+
+        //}
+
+        //public Task<Coupon> GetCouponWithError(Guid couponId)
+        //{
+        //    //not needed for this demo here
+        //    throw new NotImplementedException();
+        //}
     }
 }
